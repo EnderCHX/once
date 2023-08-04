b4be5a068cfaabc109b603a6a6b0ff8bd212cf89
EnderCHX <enderchx@outlook.com>
2023-08-04 12:20:12 +0800

    test

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
-console.log(location.search)
+getImg = async () => {
+    res = (location.search).substring(1);
+    return `https://img.chxc.cc${res}`;
+}
+
+delImg = async () => {
+    res = (location.search).substring(7);
+    console.log(res);
+    rest = await fetch(`https://img.chxc.cc/api/manage/delete/${res}`);
+    return rest;
+}
+
+getE = (id) =>{
+    return document.getElementById(id);
+}
+
+main = async () => {
+    getE(img).src = await getImg();
+    rest = delImg();
+    console.log(rest);
+}
+
+main();
